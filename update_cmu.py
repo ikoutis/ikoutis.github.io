4b28cde06309eb117037e6c31d7e13e8122a4e4e
Yiannis Koutis <i.koutis@gmail.com>
2026-04-22 19:28:14 -0400

    updated links

---
+++ update_cmu.py
@@ -0,0 +1,11 @@
+﻿import re
+
+file_path = r'njit_hosted\public_html\publications.html'
+with open(file_path, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace('http://www.cs.cmu.edu/~glmiller/Publications/TKISM-ARVO2008.pdf', 'https://ikoutis.github.io/papers/TKISM-ARVO2008.pdf')
+
+with open(file_path, 'w', encoding='utf-8') as f:
+    f.write(content)
+print('Replaced CMU link successfully.')
---
+++ update_links.py
@@ -0,0 +1,14 @@
+﻿import re
+
+file_path = r'njit_hosted\public_html\publications.html'
+with open(file_path, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Replace both ./papers/ and papers/ with https://ikoutis.github.io/papers/
+# but be careful not to double replace if it's already https://...
+content = re.sub(r'href="\./papers/', r'href="https://ikoutis.github.io/papers/', content)
+content = re.sub(r'href="papers/', r'href="https://ikoutis.github.io/papers/', content)
+
+with open(file_path, 'w', encoding='utf-8') as f:
+    f.write(content)
+print('Replaced successfully.')
